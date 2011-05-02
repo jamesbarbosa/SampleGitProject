eb6fc629988dfd4d21dc8da12e9b039e06e2c41a
James Barbosa <james.barbosa@orangeandbronze.com>
2011-05-02 10:02:17 +0800

    SAMPLE DYNAMIC PROJECT

---
+++ SAMPLE/src/com/sample/dao/daodao.java
@@ -0,0 +1,13 @@
+package com.sample.dao;
+
+public class daodao {
+	
+	
+	public static void getName(String x) {
+		System.out.println(x);
+	}
+	public static void main(String[] args) {
+		getName("POOM");
+	}
+
+}
