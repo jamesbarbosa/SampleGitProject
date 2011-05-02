719454a3f0e1090d31d3a9c697f0ce199419f148
hanselLastima <hansel.lastima@orangeandbronze.com>
2011-05-02 10:14:06 +0800

    testAddition.java sample jUnit

---
+++ SAMPLE/test/com/sample/math/testAddition.java
@@ -0,0 +1,15 @@
+package com.sample.math;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
+public class testAddition {
+	
+	@Test
+	public void testAdd() {
+		assertEquals(4, 4);
+	}
+
+}
